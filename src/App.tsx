a2f3794ca8fd4bb3e6d5ff19feb9a033716663fd
ranjit <ranjitdas2048@gmail.com>
2025-05-09 16:38:56 +0530

    build all the thing

--- src/App.tsx
+++ src/App.tsx
@@ -141,20 +141,6 @@ function App() {
               DevOps
             </li>
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="red"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="white"
-              className="size-8 opacity-80 absolute right-21 text-center top-4 margin-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
           </ul>
           <div>
             {frontEnd ? (
