d692ddf7dfc4ba8b0c995d5317bbf657df9c9b67
ranjit <ranjitdas2048@gmail.com>
2025-05-27 12:28:53 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -220,7 +220,7 @@ function App() {
         Featured <span className="text-purple-600"> Projects </span>
           </p>
       </div>
-      <div className=" px-18 pt-5 max-md:px-5  flex   max-xl:flex-wrap gap-2 max-md:gap-0   bg-[#f6f6f8] font-poppins  h-[100%] flex-wrap  w-full  items-center  ">
+      <div className=" px-18 pt-5 max-md:px-5  flex   max-xl:flex-wrap gap-8 max-md:gap-0   bg-[#f6f6f8] font-poppins  h-[100%] flex-wrap  w-full  items-center  ">
         <ProjectCard title="E-WALLET" des="A simple E-Wallet website where you can store, send , receive money using same wallet and through bank" link="https://ewallet.10xdev.shop/" code="https://github.com/ranjit1024/E-wallet"/>
 
         <ProjectCard title="BASU'S CONSULTING" des=" A Landing page that shows which service they provide for consulting firms,and helps to get dream for for candidate." link="https://habit-tracker-beta-lovat.vercel.app/" code="https://github.com/ranjit1024/personal-project"/>
@@ -233,7 +233,7 @@ function App() {
 
       <div className=" flex flex-col h-1    ">
 
-      <div className="w-10 h-10 p-2 font-poppins border-2  top-10 border-yellow-500 max-md:top-11  rounded-full max-md:left-2 left-5  relative bg-[#f6f6f8] flex items-center justify-center shadow-sm shadow-yellow-200 ">
+      <div className="w-10 h-10 p-2 font-poppins border-2  top-12 border-yellow-500 max-md:top-11  rounded-full max-md:left-2 left-5  relative bg-[#f6f6f8] flex items-center justify-center shadow-sm shadow-yellow-200 ">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
 </svg>
@@ -241,7 +241,7 @@ function App() {
       </div>
 
       <div className="flex pt-20  ">
-        <p className="font-[600] left-20 -top-20 max-md:-top-20  relative font-poppins  text-[2.1rem] max-md:left-17 text-black">
+        <p className="font-[600] left-20 -top-19 max-md:-top-20  relative font-poppins  text-[2.1rem] max-md:left-17 text-black">
         Get <span className="text-yellow-600"> In Touch </span>
           </p>
       </div>
